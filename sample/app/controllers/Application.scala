4aa3c81f9f80f8c8e0269a5d0565ae90d19b5157
Carlos Quiroz <cquiroz@ketalo.com>
2013-04-16 17:09:23 -0300

    Added Javascript files and templates for a pre2 sample

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def pre2 = Action {
+    Ok(views.html.pre2())
+  }
   
 }
