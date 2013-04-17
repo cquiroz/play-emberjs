def13af490fa313b1c5d1d16938c87ffa901202e
Carlos Quiroz <cquiroz@ketalo.com>
2013-04-17 10:07:09 -0300

    Remove unused templates

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -5,10 +5,6 @@ import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
   def pre2 = Action {
     Ok(views.html.pre2())
   }
